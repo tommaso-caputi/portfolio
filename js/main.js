61d99a065c7c81f2401f2ad39943c58b24e3dbc5
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-03-23 10:41:48 +0100

    implement buttons

--- js/main.js
+++ js/main.js
@@ -9,3 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
         aboutLink.classList.add("active");
     }
 });
+
+
+const opencv = () => {
+    window.open("https://drive.google.com/file/d/1y6_BxV_v1OdBd8CiKRqi5Pvk4vRW_BX9/view?usp=sharing");
+}
+
+const openemail = () => {
+    var email = 'tommasocaputi85@gmail.com';
+    var subject = '';
+    var emailBody = '';
+    window.open("mailto:" + email + "?subject=" + subject + "&body=" + emailBody);
+}
