69bf0b34cabcd32d2d75a9e38787ad0671f10310
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-08-15 22:33:34 +0200

    set terminal href

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -5,7 +5,7 @@ import { FaTerminal } from "react-icons/fa";
 export default function Header() {
     return (
         <header className="px-8 lg:px-6 h-14 flex items-center">
-            <Link href="#" className="flex items-center justify-center" prefetch={false}>
+            <Link href="https://tommasocaputi.altervista.org/portfolio/terminal/index.html" className="flex items-center justify-center" prefetch={false}>
                 <FaTerminal className="size-6" />
                 <span className="sr-only">Tommaso Caputi's portfolio</span>
             </Link>
