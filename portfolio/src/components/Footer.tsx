038922a41243fc378c81505bddef25d8ca874e44
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-09-11 14:05:06 +0200

    quick fix

--- portfolio/src/components/Footer.tsx
+++ portfolio/src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer>
-            <p className="font-medium text-lg pb-6">Experience</p>
+            <p className="font-medium text-lg pb-6">Contact</p>
             <div className="flex flex-row space-x-4">
                 <a href="mailto:tommasocaputi85@gmail.com" className="flex flex-row">
                     <p className="underline underline-offset-4 font-medium ">Email</p>&#8599;
