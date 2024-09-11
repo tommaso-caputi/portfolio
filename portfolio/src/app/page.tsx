d45e608b4981128a94c49b948cd79f3a5cfe455b
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-09-11 12:23:58 +0200

    quick fix

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="flex justify-center min-h-screen py-8">
+    <div className="flex justify-center min-h-screen py-7">
       <div className="space-y-28 pt-8 text-left px-3 md:w-7/12 sm:w-full">
         <FirstSection />
         <ProjectsSection />
