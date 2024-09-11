917c36a92990bb3faf56f78eefcd52bb6d037385
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-09-11 14:07:29 +0200

    fix resize

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen py-7">
-      <div className="space-y-28 pt-8 text-left px-3  sm:w-8/12 md:w-5/12 lg:w-5/12">
+      <div className="space-y-28 pt-8 text-left px-3  sm:w-8/12 md:w-8/12 lg:w-5/12">
         <FirstSection />
         <ProjectsSection />
         <ExperienceSection />
