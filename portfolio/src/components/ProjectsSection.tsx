af0762788d02532663cefdd6dfd9f6a6755d2f3d
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-09-13 11:10:34 +0200

    quick fix

--- portfolio/src/components/ProjectsSection.tsx
+++ portfolio/src/components/ProjectsSection.tsx
@@ -10,7 +10,7 @@ export default function ProjectsSection() {
             tryItOutUrl: 'https://cryptokeeper.altervista.org/'
         },
         {
-            title: 'Delivery app',
+            title: "Masseria Coppa's delivery app",
             description: "The adoption of delivery management software for handling orders and deliveries has improved the company's operations by 70%.",
             period: '2023 - present',
             imageUrl: '/LogoDeliveryApp.png'
