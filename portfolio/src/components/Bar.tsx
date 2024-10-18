aa9b8589168459911788310ccc20ee00bdd310b4
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-10-18 18:06:15 +0200

    changed icons

--- portfolio/src/components/Bar.tsx
+++ portfolio/src/components/Bar.tsx
@@ -3,6 +3,8 @@
 import React, { useState } from 'react';
 import { FaGithub } from "react-icons/fa";
 import { SlNotebook } from "react-icons/sl";
+import { FiHome } from "react-icons/fi";
+
 
 
 export default function Bar() {
@@ -52,21 +54,8 @@ export default function Bar() {
                         data-state="closed"
                         href="/"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-home size-4"
-                        >
-                            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                        </svg>
+                        <FiHome size="18" />
+
                     </a>
                     {hoveredIcon === 'home' && popover('Home')}
                 </div>
@@ -82,25 +71,7 @@ export default function Bar() {
                         data-state="closed"
                         href="/blog"
                     >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            className="lucide lucide-notebook size-4"
-                        >
-                            <path d="M2 6h4"></path>
-                            <path d="M2 10h4"></path>
-                            <path d="M2 14h4"></path>
-                            <path d="M2 18h4"></path>
-                            <rect width="16" height="20" x="4" y="2" rx="2"></rect>
-                            <path d="M16 2v20"></path>
-                        </svg>
+                        <SlNotebook size="18" />
                     </a>
                     {hoveredIcon === 'blog' && popover('Blog')}
                 </div>
@@ -115,9 +86,9 @@ export default function Bar() {
                     <a
                         className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
                         data-state="closed"
-                        href="https://github.com"
+                        href="https://github.com/tommaso-caputi"
                     >
-                        <FaGithub />
+                        <FaGithub size={18} />
                     </a>
                     {hoveredIcon === 'github' && popover('GitHub')}
                 </div>
