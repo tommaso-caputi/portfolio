194a95506bd82f76a1b47a270f7c25eee1da6667
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-10-18 17:50:33 +0200

    fix popover and hide footer

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -7,11 +7,11 @@ import Bar from "@/components/Bar";
 export default function Home() {
   return (
     <div className="flex justify-center min-h-screen py-7">
-      <div className="space-y-28 pt-8 text-left px-3 sm:w-8/12 md:w-8/12 lg:w-5/12">
+      <div className="space-y-28 pt-8 text-left px-3 sm:w-8/12 md:w-8/12 lg:w-5/12 mb-36">
         <FirstSection />
         <ProjectsSection />
         <ExperienceSection />
-        <Footer />
+        {/* <Footer /> */}
       </div>
       <Bar />
     </div>
--- portfolio/src/components/Bar.tsx
+++ portfolio/src/components/Bar.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
 import React, { useState } from 'react';
 
 export default function Bar() {
     const [barWidth, setBarWidth] = useState(0);
+    const [hoveredIcon, setHoveredIcon] = useState(null); // State to track which icon is hovered
 
     const handleMouseEnter = () => {
         setBarWidth((prevWidth) => prevWidth + 25);
@@ -13,6 +14,14 @@ export default function Bar() {
         setBarWidth((prevWidth) => Math.max(prevWidth - 25, 0));
     };
 
+    const handleIconMouseEnter = (icon: any) => {
+        setHoveredIcon(icon);
+    };
+
+    const handleIconMouseLeave = () => {
+        setHoveredIcon(null);
+    };
+
     return (
         <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
             <div
@@ -20,14 +29,17 @@ export default function Bar() {
                             flex min-h-full h-full items-center px-1 bg-background justify-around
                             [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] 
                             transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] 
-                            transition-all duration-100 ease-in-out`}
+                            transition-all duration-200 ease-in-out`}
                 onMouseEnter={handleMouseEnter}
                 onMouseLeave={handleMouseLeave}
                 style={{ width: `${100 + barWidth}px` }}
             >
+                {/* Home Icon */}
                 <div
-                    className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300"
+                    className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300 relative"
                     style={{ width: "40px", height: "40px" }}
+                    onMouseEnter={() => handleIconMouseEnter('home')}
+                    onMouseLeave={handleIconMouseLeave}
                 >
                     <a
                         className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium 
@@ -53,11 +65,17 @@ export default function Bar() {
                             <polyline points="9 22 9 12 15 12 15 22"></polyline>
                         </svg>
                     </a>
+                    {hoveredIcon === 'home' && (
+                        popover('Home')
+                    )}
                 </div>
 
+                {/* Blog Icon */}
                 <div
-                    className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300"
+                    className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300 relative"
                     style={{ width: "40px", height: "40px" }}
+                    onMouseEnter={() => handleIconMouseEnter('blog')}
+                    onMouseLeave={handleIconMouseLeave}
                 >
                     <a
                         className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
@@ -84,8 +102,20 @@ export default function Bar() {
                             <path d="M16 2v20"></path>
                         </svg>
                     </a>
+                    {hoveredIcon === 'blog' && (
+                        popover('Blog')
+                    )}
                 </div>
             </div>
         </div>
+    );
+}
+
+function popover(name: string) {
+    return (
+        < div className="absolute bottom-full mb-2 left-1/2 transform -translate-x-1/2 
+                         p-1.5 rounded shadow-lg text-white bg-black text-sm" >
+            {name}
+        </div >
     );
 }
