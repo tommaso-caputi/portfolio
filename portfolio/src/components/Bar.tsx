85da279ac2ea0e6eaf250decbc201de3a7e7ba85
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-10-18 17:22:28 +0200

    good point

--- portfolio/src/components/Bar.tsx
+++ portfolio/src/components/Bar.tsx
@@ -1,11 +1,19 @@
 export default function Bar() {
     return (
         <div className="pointer-events-none fixed inset-x-0 bottom-0 z-30 mx-auto mb-4 flex origin-bottom h-full max-h-14">
-            <div className="bg-white p-2 rounded-full border z-50 pointer-events-auto relative mx-auto flex min-h-full h-full items-center px-1 bg-background [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] transition-all duration-300 hover:px-10">
+            <div className="bg-white p-2 rounded-full border z-50 pointer-events-auto relative mx-auto 
+                            flex min-h-full h-full items-center px-1 bg-background justify-around
+                            [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] 
+                            transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset] 
+                            transition ease-in-out hover:w-40 duration-1000 delay-150">
 
-                <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300" style={{ width: "40px" }}>
+                <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full 
+                                hover:bg-gray-200 transition-all duration-300" style={{ width: "40px", height: "40px" }}>
                     <a
-                        className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
+                        className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium 
+                                   transition-colors focus-visible:outline-none focus-visible:ring-1 
+                                   focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 
+                                   hover:bg-accent hover:text-accent-foreground rounded-full size-12"
                         data-state="closed"
                         href="/"
                     >
@@ -27,7 +35,7 @@ export default function Bar() {
                     </a>
                 </div>
 
-                <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300" style={{ width: "40px" }}>
+                <div className="flex aspect-square cursor-pointer items-center justify-center rounded-full hover:bg-gray-200 transition-all duration-300" style={{ width: "40px", height: "40px" }}>
                     <a
                         className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground rounded-full size-12"
                         data-state="closed"
