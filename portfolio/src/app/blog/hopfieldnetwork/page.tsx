eb7e968e8b4dede9e656f11cd6836fe09950c773
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-10-18 21:37:08 +0200

    fix pageLoader for post1

--- portfolio/src/app/blog/hopfieldnetwork/page.tsx
+++ portfolio/src/app/blog/hopfieldnetwork/page.tsx
@@ -4,13 +4,13 @@ import Bar from "@/components/Bar";
 import pageLoader from "@/components/pageLoader";
 
 export default function Post1() {
-    const sections = 1;
+    const sections = 2;
     const visibleSections = pageLoader(sections, 400);
 
     return (
         <div className="flex justify-center min-h-screen py-7">
             <div className="space-y-10 pt-8 text-left px-3 sm:w-8/12 md:w-8/12 lg:w-8/12 mb-36">
-                <div>
+                <div className={`transition-opacity duration-[1000ms] ${visibleSections[0] ? 'opacity-100' : 'opacity-0'}`}>
                     <div className="flex">
                         <div className="space-y-4 w-11/12">
                             <p style={{ fontSize: '17px' }} className='font-medium'>Hopfield Network Python implementation</p>
