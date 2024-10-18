8c87a323ec9dcdaafd89c0bdbaddc01aa594ff34
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-10-18 18:23:51 +0200

    add blog page

---
+++ portfolio/src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import Bar from "@/components/Bar";
+
+export default function Blog() {
+    return (
+        <div className="flex justify-center min-h-screen py-7">
+            <div className="space-y-28 pt-8 text-left px-3 sm:w-8/12 md:w-8/12 lg:w-5/12 mb-36">
+                Blog
+            </div>
+            <Bar />
+        </div>
+    );
+}
