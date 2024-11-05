ac5a12e59ba2407e37b2bd31eaa34257cc4bcc16
tommaso caputi <56504768+tommaso-caputi@users.noreply.github.com>
2024-11-05 23:43:40 +0100

    start adding new post

--- portfolio/src/mdx-components.tsx
+++ portfolio/src/mdx-components.tsx
@@ -1,9 +1,32 @@
-import type { MDXComponents } from 'mdx/types'
+import type { MDXComponents } from 'mdx/types';
+import Image, { ImageProps } from 'next/image';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
     return {
         h1: ({ children }) => (
             <h1 className="text-4xl md:text-5xl font-bold pb-10">{children}</h1>
-        )
-    }
+        ),
+        h2: ({ children }) => (
+            <h3 className="subparagraph-title">{children}</h3>
+        ),
+        h3: ({ children }) => (
+            <h3 className="subparagraph-title2">{children}</h3>
+        ),
+        img: (props) => {
+            const { width = 100, height = 100, ...rest } = props as ImageProps & { width?: number; height?: number };
+            return (
+                <Image
+                    width={width}
+                    height={height}
+                    style={{ width: '100%', height: 'auto' }}
+                    {...rest}
+                />
+            );
+        },
+        p: ({ children }) => <p className="normal-text">{children}</p>,
+
+        ol: ({ children }) => <ol className="numbered-list pl-5">{children}</ol>,
+        ul: ({ children }) => <ul className="list-disc pl-5">{children}</ul>,
+        li: ({ children }) => <li className="mb-2">{children}</li>,
+    };
 }
